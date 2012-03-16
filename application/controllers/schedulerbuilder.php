5c7ee710d2f08d54989cfec46a23fb25bbce8660
nimonikMike <viet_mike.pham@hotmail.com>
2012-03-15 20:10:12 -0400

    nothing important probably going to delete these

--- application/controllers/schedulerbuilder.php
+++ application/controllers/schedulerbuilder.php
@@ -1,9 +1,17 @@
 <?php
 
-class SchedulerBuilder extends CI_Controller {
+class SchedulerBuilder extends CI_Controller{
 
-	public function index()
+
+	public function listAllCourses()
 	{
+        $this->load->helper('form');
+        // course_list is used for now to display all course seletion for user. This will be replaces with actual returned record from course table.
+        $this->config->load('pasta_constants/course_list');
+        $data = array( 'courseList' => $this->config->item('COURSE_LIST') );
+        $this->load->view('listAllCourses.php', $data);
 	}
 
 }
+
+?>
