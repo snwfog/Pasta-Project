1dcc71bb62a26b9f0638b7e9fe68615827b1161e
nimonikMike <viet_mike.pham@hotmail.com>
2012-03-15 21:18:51 -0400

    autoload pasta dabase, added course model

--- application/controllers/schedulerbuilder.php
+++ application/controllers/schedulerbuilder.php
@@ -5,10 +5,8 @@ class SchedulerBuilder extends CI_Controller{
 
 	public function listAllCourses()
 	{
-        $this->load->helper('form');
-        // course_list is used for now to display all course seletion for user. This will be replaces with actual returned record from course table.
-        $this->config->load('pasta_constants/course_list');
-        $data = array( 'courseList' => $this->config->item('COURSE_LIST') );
+        $this->load->model('course_model');
+        $data = array('courseList' => $this->course_model->get_all_courses());
         $this->load->view('listAllCourses.php', $data);
 	}
 
