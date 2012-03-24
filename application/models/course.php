e23d61cc8c1ce87edf87a7b57b704bde68f2a9bd
snw <donchoa@gmail.com>
2012-03-24 00:46:57 -0400

    Remove type checks in Course model
    Remove the type checks and errors in the course model, because
    need to use "String" to better query results from array of strings.

--- application/models/course.php
+++ application/models/course.php
@@ -13,31 +13,35 @@ class Course extends CI_Model{
     function find_by_id($id)
     {
       if (FALSE === is_int($id)) { // only allow id of type integer
-        trigger_error('setInteger expected Argument 1 to be Integer', E_USER_WARNING);
+        trigger_error('setInteger expected Argument 1 to be Integer', 
+          E_USER_WARNING);
       }
       $query = $this->db->get_where('courses', array('id' => $id));
-      return $query->row_array(); //row_array returns a single result in a pure array. Better for generating single results.
+      
+      // row_array returns a single result in a pure array. 
+      // Better for generating single results.
+      return $query->row_array(); 
     }
 
     function find_by_code($code)
     {
       if (FALSE === is_string($code)) {
-        trigger_error('setString expected Argument 1 to be String', E_USER_WARNING);
+        trigger_error('setString expected Argument 1 to be String', 
+          E_USER_WARNING);
       }
       $query = $this->db->get_where('courses', array('code' => $code));
-      return $query->result_array(); //result_array() return multiple result in a pure array.
+
+      //result_array() return multiple result in a pure array.
+      return $query->result_array(); 
     }
 
-    function find_by_code_number($code, $number)
-    { 
-      if (FALSE === is_string($code)) {
-        trigger_error('setString expected Argument 1 to be String', E_USER_WARNING);
-      }
-      if (FALSE === is_int($number)) {
-        trigger_error('setString expected Argument 2 to be Integer', E_USER_WARNING);
-      }
-      $query = $this->db->get_where('courses', array('code' => $code, 'number' => $number));
-      return $query->row_array(); // used row_array() because there is no two record with same code and number.
+    function find_by_code_number($code, $number) { 
+      $query = $this->db->get_where('courses', 
+        array('code' => $code, 'number' => $number));
+
+      // use row_array() because there 
+      // is no two record with same code and number.
+      return $query->row_array(); 
     }
 }
 ?>
