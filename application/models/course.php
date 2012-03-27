6ccbeb75d692ef2b2dce9cc1d2128a45f369aba4
snw <donchoa@gmail.com>
2012-03-27 17:10:54 -0400

    Add credit attribute to user profile page

--- application/models/course.php
+++ application/models/course.php
@@ -2,29 +2,21 @@
 class Course extends CI_Model{
   //NOTE: ADD TITLE COLUMN FOR COURSE TABLE
 
-    function get_all_courses()
-    {
-      $this->db->select("id, code, number, credit");
-      $query = $this->db->get('courses');
-      return $query->result_array();
+    function get_all_courses() {
+        $this->db->select("id, code, number, credit, 'title");
+        $query = $this->db->get('courses');
+        return $query->result_array();
 
     }
 
-    function find_by_id($id)
-    {
-      if (FALSE === is_int($id)) { // only allow id of type integer
-        trigger_error('setInteger expected Argument 1 to be Integer', 
-          E_USER_WARNING);
-      }
-      $query = $this->db->get_where('courses', array('id' => $id));
-      
-      // row_array returns a single result in a pure array. 
-      // Better for generating single results.
-      return $query->row_array(); 
+    function find_by_id($id) {
+        $query = $this->db->get_where('courses', array('id' => $id));
+        // row_array returns a single result in a pure array. 
+        // Better for generating single results.
+        return $query->row_array(); 
     }
 
-    function find_by_code($code)
-    {
+    function find_by_code($code) {
       if (FALSE === is_string($code)) {
         trigger_error('setString expected Argument 1 to be String', 
           E_USER_WARNING);
