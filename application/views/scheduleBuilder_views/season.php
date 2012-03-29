9c06eaa222c2c969b671ad248d842c5dfcf3be2a
nimonikMike <viet_mike.pham@hotmail.com>
2012-03-28 23:55:55 -0400

    forgot git add

---
+++ application/views/scheduleBuilder_views/season.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Choose Your Courses for this semester</title>
+</head>
+<body>
+
+<?php echo anchor(site_url("/scheduleBuilder/listAllAllowedCourses/2"), "Make Fall Semester Schedule");?>
+<br/>
+<?php echo anchor(site_url("/scheduleBuilder/listAllAllowedCourses/4"), "Make Winter Semester Schedule");?>
+</body>
+</html>
