b70cb0fc9f7f2c3baa19048c91857dbeea4772a2
snw <donchoa@gmail.com>
2012-03-29 20:53:41 -0400

    Play with font on Pasta main

--- application/views/static/footer.php
+++ application/views/static/footer.php
@@ -1,8 +1,7 @@
 	<!-- P.A.S.T.A. footer -->
 	<div id="footer">
-		<hr>
 		<p>Project SOEN 341</p>
-		<p>Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+		<!-- <p>Page rendered in <strong>{elapsed_time}</strong> seconds</p> -->
 	</div>
 <!-- id = outter -->
 </div>
