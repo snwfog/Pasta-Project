f25b327f1e5e813ecfdc8c4061d61598dee53b98
nimonikMike <viet_mike.pham@hotmail.com>
2012-04-04 00:18:47 -0400

    css

--- application/views/scheduleBuilder_views/generated_schedule.php
+++ application/views/scheduleBuilder_views/generated_schedule.php
@@ -1,9 +1,6 @@
 <div id="content">
 
-  <?php   //TO DO, option to choose which schedule and save to database.
-          // EITHER DISPLAY by text all possible sets of courses and a link to display in a time table or
-          // display all the time table with a button beside it to save this schedule
-  
+  <?php
           //THIS should be moved to a helper class. helper class help views? maybe dono.
     function get_hour_min($time){
       //Note: should be moved to helper class
@@ -84,29 +81,7 @@
   </div>
 
    <!-- For testing purpose
-
-       <?php
-        foreach($possible_sequence as $sequence){
-          echo "<hr/><br/>";
-          foreach($sequence as $course){
-            echo $course["id"]." ".$course["code"].$course["number"].
-                 "<br/> ".$course["lecture"]["id"]." Lecture(".$course["lecture"]["section"].") ".$course["lecture"]["start_time"]."-".$course["lecture"]["end_time"].$course["lecture"]["day"]."<br/>";
-            if(isset($course["tutorial"])){
-              echo $course["tutorial"]["id"]."tutorial(".$course["tutorial"]["section"].") ".$course["tutorial"]["start_time"]."-".$course["tutorial"]["end_time"].$course["tutorial"]["day"]."<br/>";
-            }
-            if(isset($course["lab"])){
-              echo "lab(".$course["lab"]["section"].")";
-            }
-            echo "<br/>";
-          }
-        echo "End of One Sequence <br/><br/>";
-        echo "<hr/><br/>";
-        }
-      ?>
-      
-
-
-         <?php  $data["time_table"] = $time_table;
+    <?php  $data["time_table"] = $time_table;
                 $this->load->view("/scheduleBuilder_views/time_table.php", $data);
          ?>
      --!>
