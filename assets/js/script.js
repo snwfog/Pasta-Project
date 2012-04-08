bfc556da2cf6fa1099b14042d0d85d4e0b5652e9
snw <donchoa@gmail.com>
2012-04-07 21:22:06 -0400

    Anotated javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 /**
  * PASTA - SOEN341 Concordia 2012
  * JavaScript functions
+ * 
+ * Author: Charles Yang
+ * Date:   Feburary 2012
  */
 $(document).ready(function() {
 	/**
@@ -186,31 +189,3 @@ $(document).ready(function() {
 
 
 });
-
-// // extend jquery ui accordion to allow multiple
-// // sections at once if the multiple option is true
-// // from http://forum.jquery.com/topic/accordion-multiple-sections-open-at-once
-// $.extend($.ui.accordion.prototype.options,{multiple: false});
-// var _toggle = $.ui.accordion.prototype._toggle;
-// var _clickHandler = $.ui.accordion.prototype._clickHandler;
-// $.extend($.ui.accordion.prototype,{
-// 	_toggle: function(toShow, toHide, data, clickedIsActive, down){
-// 		if (this.options.collapsible && this.options.multiple && toShow.is(':visible')) {
-// 			arguments[1] = arguments[0];
-// 			arguments[3] = true;
-// 		}
-// 		else if (this.options.collapsible && this.options.multiple) {
-// 			arguments[1] = $([]);			
-// 		}
-// 		_toggle.apply(this,arguments);
-// 		this.active
-// 			.removeClass( "ui-state-active ui-corner-top" )
-// 			.addClass( "ui-state-default ui-corner-all" )
-// 	},
-// 	_clickHandler: function(event, target){
-// 		if ($(target).next().is(':visible:not(:animated)')) {
-// 			this.active = $(target);
-// 		}
-// 		_clickHandler.apply(this,arguments)
-// 	}
-// });
