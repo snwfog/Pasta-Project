5cad5eac08ae6d77883f63011ceef738a5233fd4
Eric Rideough <erideough@gmail.com>
2012-04-09 12:09:44 -0400

    Display the current date in the footer.

--- application/views/static/footer.php
+++ application/views/static/footer.php
@@ -7,7 +7,7 @@
 		<div id="page-1"></div>
 		<div id="page-2"></div>
 		<div id="page-3"></div>
-		<h5>Project SOEN 341 - April 5, 2012</h5>
+		<h5>Project SOEN 341 - <?php echo date("F j, Y") ?></h5>
 
 		<!-- <p>Page rendered in <strong>{elapsed_time}</strong> seconds</p> -->
 	</div><!-- #footer -->
